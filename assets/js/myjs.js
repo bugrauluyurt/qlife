47ec67a84f52ff6497eb9ff2f444864cf431acbd
bugrauluyurt <bugra.u@gmail.com>
2016-05-13 21:10:06 +0300

    Finished navbar and main-section

---
+++ assets/js/myjs.js
@@ -0,0 +1,21 @@
+/* Write here your custom javascript codes */
+jQuery(document).ready(function() {
+    $('.logo-attach-2').hide();
+    if ($.browser.desktop && $(window).width()>1024) {
+        if ($('#dna-image').hasClass('hidden')) {
+            $('#dna-image').removeClass('hidden');
+        }
+        $(window).scroll(function() {
+            if ($(this).scrollTop()>0)
+             {
+                $('.logo-attach-1').hide();
+                $('.logo-attach-2').show();
+             } else {
+                $('.logo-attach-1').show();
+                $('.logo-attach-2').hide();
+             }
+        });
+    } else {
+        $('#dna-image').addClass('hidden');
+    }
+});
